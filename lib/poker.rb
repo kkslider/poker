28e8b93b089db65df82a993e54a8840840f56967
Benjamin Lee & Brian Kwon <benjamsandwich@gmail.com>
2013-09-27 18:13:26 -0700

    Started implementing Poker hand methods

--- lib/poker.rb
+++ lib/poker.rb
@@ -33,6 +33,108 @@ class Deck
 end
 
 class Hand
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+  end
+
+  def <=>(other_hand)
+    # -1, 0, 1
+  end
+
+  def type # [:Full_House, 10]
+    return check_royal_flush if check_royal_flush
+    return check_straight_flush if check_straight_flush
+    return check_four_of_a_kind if check_four_of_a_kind
+    return check_full_house if check_full_house
+    return check_flush if check_flush
+    return check_straight if check_straight
+    # :Straight if straight?
+    # :Three_of_a_Kind if three_of_a_kind?
+    # :Two_Pair if two_pair?
+    # :Pair if pair?
+  end
+  def combo
+    type[0]
+  end
+
+  def check_royal_flush
+    ranks = []
+    suits = []
+
+    @cards.each do |card|
+      ranks << card.rank
+      suits << card.suit
+    end
+
+    ranks = ranks.sort
+    ranks.each_with_index do |rank, index|
+      if rank != index + 10
+        return nil
+      end
+    end
+
+    return nil if suits.uniq.length != 1
+    [:Royal_Flush, suits[0]]
+  end
+
+  def check_straight_flush
+    ranks = []
+    suits = []
+    temp_ranks = []
+
+    @cards.each do |card|
+      ranks << card.rank
+      suits << card.suit
+    end
+
+    rank = ranks.sort
+
+    ranks.each do |rank|
+      temp_ranks << rank - ranks[0]
+    end
+
+    return nil if temp_ranks != [0, 1, 2, 3, 4] || suits.uniq.length != 1
+    [:Straight_Flush, suits[0]]
+  end
+
+  def check_four_of_a_kind
+    ranks = @cards.map { |card| card.rank }
+    kind = ranks.sort[2]
+    ranks.delete(kind)
+    return nil if ranks.length != 1
+
+    [:Four_of_a_Kind, kind]
+  end
+
+  def check_full_house
+    ranks = @cards.map { |card| card.rank }
+    kind = ranks.sort[2]
+    ranks.delete(kind)
+    ranks.delete(ranks[0])
+    return nil if ranks.length != 0
+
+    [:Full_House, kind]
+  end
+
+  def check_flush
+    ranks = []
+    suits = []
+
+    @cards.each do |card|
+      ranks << card.rank
+      suits << card.suit
+    end
+
+    return nil if suits.uniq.length != 1
+    [:Flush, ranks.sort.last]
+  end
+
+  def check_straight
+
+  end
+
 
 end
 
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/poker'
 
 describe Card do
   #hash? for num
-  subject(:card) { Card.new(5, :Hearts) }
+  subject(:card) { Card.new(5, :heart) }
 
   describe "#initialize" do
     it "card.rank should equal 5" do
@@ -11,7 +11,7 @@ describe Card do
     end
 
     it "card.suit should equal :heart" do
-      expect(card.suit).to eq(:Hearts)
+      expect(card.suit).to eq(:heart)
     end
   end
 
@@ -47,9 +47,9 @@ describe Hand do
   subject(:hand) { Hand.new }
 
   describe "#initialize" do
-    it "@cards should be an array of length 5" do
-      expect(hand.cards.length).to eq(5)
-    end
+    # it "@cards should be an array of length 5" do
+#       expect(hand.cards.length).to eq(5)
+#     end
 
     it "@cards should all be Card objects" do
       expect{ hand.cards.all? { |card| card.class == Card } }.to be_true
@@ -58,55 +58,61 @@ describe Hand do
 
   describe "#combo" do
     it "Hand with A_Diamond, K_Diamond, Q_Diamond, J_Diamond, 10_Diamond" do
-      hand.cards = [Card.new(14, :Diamonds), Card.new(13, :Diamonds), Card.new(12, :Diamonds), Card.new(11, :Diamonds), Card.new(10, :Diamonds)]
+      hand.cards = [Card.new(14, :diamond), Card.new(13, :diamond), Card.new(12, :diamond), Card.new(11, :diamond), Card.new(10, :diamond)]
       expect(hand.combo).to eq(:Royal_Flush)
     end
 
     it "Hand with 3_Club, 3_Spade, 3_Diamond, 6_Club, 6_Heart should be full house" do
-      hand.cards = [Card.new(3, :Clubs), Card.new(3, :Spades), Card.new(3, :Diamonds), Card.new(6, :Clubs), Card.new(6, :Hearts)]
+      hand.cards = [Card.new(3, :club), Card.new(3, :spade), Card.new(3, :diamond), Card.new(6, :club), Card.new(6, :heart)]
       expect(hand.combo).to eq(:Full_House)
     end
 
     it "Hand with a 10_Spade, 10_Heart, 3_Club, 4_Heart, 8_Diamond should be pair" do
-      hand.cards = [Card.new(10, :Spades), Card.new(4, :Hearts), Card.new("A", :Clubs), Card.new(10, :Hearts), Card.new(3, :Diamonds)]
+      hand.cards = [Card.new(10, :spade), Card.new(4, :heart), Card.new("A", :club), Card.new(10, :heart), Card.new(3, :diamond)]
       expect(hand.combo).to eq(:Pair)
     end
 
     it "Hand with Q_Club, 10_Club, 7_Club, 6_Club, 4_Club should be flush" do
-      hand.cards = [Card.new(12, :Clubs), Card.new(10, :Clubs), Card.new(7, :Clubs), Card.new(6, :Clubs), Card.new(4, :Clubs)]
+      hand.cards = [Card.new(12, :club), Card.new(10, :club), Card.new(7, :club), Card.new(6, :club), Card.new(4, :club)]
       expect(hand.combo).to eq(:Flush)
     end
 
     it "Hand with a 6_Heart, 7_Club, 8_Diamond, 9_Club, 10_Spade should be a straight" do
-      hand.cards = [Card.new(6, :Hearts), Card.new(7, :Clubs), Card.new(8, :Diamonds), Card.new(9, :Clubs), Card.new(10, :Spades)]
+      hand.cards = [Card.new(6, :heart), Card.new(7, :club), Card.new(8, :diamond), Card.new(9, :club), Card.new(10, :spade)]
       expect(hand.combo).to eq(:Straight)
     end
 
-    it "Hand with 4_Heart, 10_Jack, A_Club, 5_Heart, 2_Diamond should return nil" do
-      hand.cards = [Card.new(4, :Hearts), Card.new(10, :Jacks), Card.new(14, :Clubs), Card.new(5, :Hearts), Card.new(2, :Diamonds)]
-      expect(hand.combo).to eq(nil)
+    it "Hand with 4_Heart, 10_Heart, A_Club, 5_Heart, 2_Diamond should return nil" do
+      high_card = Card.new(14, :club)
+      hand.cards = [Card.new(4, :heart), Card.new(10, :heart), high_card, Card.new(5, :heart), Card.new(2, :diamond)]
+      expect(hand.combo).to eq(high_card)
     end
 
     it "Hand with 9Club, 9_Spade, 9_Diamond, 9_Heart, J_Heart" do
-      hand.cards = [Card.new(9, :Clubs), Card.new(9, :Spades), Card.new(9, :Diamonds), Card.new(9, :Hearts), Card.new(11, :Heart)]
+      hand.cards = [Card.new(9, :club), Card.new(9, :spade), Card.new(9, :diamond), Card.new(9, :heart), Card.new(11, :Heart)]
       expect(hand.combo).to eq(:Four_of_a_Kind)
     end
   end
 
-  describe "#hand_rankings" do
-    it "Royal Flush should be ranked with a higher(larger) value than a Two Pair" do
-      expect(hand.hand_rankings["Royal Flush"] > hand.hand_rankings["Two Pair"]).to be_true
-    end
-
-    it "Full House should be ranked with a higher(larger) value than a Flush" do
-      expect(hand.hand_rankings["Full House"] > hand.hand_rankings["Flush"]).to be_true
-    end
-  end
+  # describe "#hand_rankings" do
+  #   let(:royal_flush_hand)
+  #   let(:two_pair_hand)
+  #
+  #
+  #   it "Royal Flush should be ranked with a higher(larger) value than a Two Pair" do
+  #     expect(royal_flush_hand > two_pair_hand).to be_true
+  #     expect(hand.hand_rankings["Royal Flush"] > hand.hand_rankings["Two Pair"]).to be_true
+  #   end
+  #
+  #   it "Full House should be ranked with a higher(larger) value than a Flush" do
+  #     expect(hand.hand_rankings["Full House"] > hand.hand_rankings["Flush"]).to be_true
+  #   end
+  # end
 
   describe "#high" do
     it "Hand with 3_Diamond, 4_Heart, 10_Spade, 6_Club, 7_Heart to return 10_Spade" do
-      high_card = Card.new(10, :Spades)
-      hand.cards = [Card.new(3, :Diamonds), Card.new(4, :Hearts), high_card, Card.new(6, :Clubs), Card.new(7, :Hearts)]
+      high_card = Card.new(10, :spade)
+      hand.cards = [Card.new(3, :diamond), Card.new(4, :heart), high_card, Card.new(6, :club), Card.new(7, :heart)]
       expect(hand.high).to eq(high_card)
     end
   end
